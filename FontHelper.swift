fc57d0d76efe56dea05a652c7512c77ab8086621
Desmond <kohdesmond@gmail.com>
2015-10-30 10:24:48 +0800

    Create FontHelper.swift

---
+++ FontHelper.swift
@@ -0,0 +1,13 @@
+//
+//  FontHelper.swift
+//
+//  Created by Desmond Koh on 25/09/2015.
+//
+
+import UIKit
+
+    // MARK: - Font
+     func textFontWithSize(fSize: CGFloat) -> UIFont {
+        return UIFont(name: "ProximaNova-Regular", size: fSize)!
+    }
+
